0348a3b74efdf04f8cb9a57d988d46d4090af4ff
TadCordle <tcordle1@umbc.edu>
2016-07-22 22:34:44 -0400

    Start working on ship controls

---
+++ Assets/Scripts/RaceCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceCamera : MonoBehaviour {
+
+    public GameObject Target;
+    public float MaxDistance;
+
+    private Vector3 Velocity;
+    new Transform transform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform = GetComponent<Transform>();
+        Velocity = Vector3.zero;
+        MaxDistance = 20;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Target != null)
+        {
+            Transform targetTransform = Target.GetComponent<Transform>();
+            transform.LookAt(targetTransform);
+            Vector3 posDiff = targetTransform.position - transform.position;
+            if (posDiff.sqrMagnitude > MaxDistance * MaxDistance)
+            {
+                Vector3 towards = posDiff.x * Vector3.right + posDiff.z * Vector3.forward;
+                Velocity += towards.normalized;
+            }
+            else if (posDiff.sqrMagnitude < MaxDistance * MaxDistance - 5)
+            {
+                Velocity *= 0.9f;
+            }
+        }
+
+        transform.position += Velocity * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/ShipControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipControls : MonoBehaviour {
+
+    new Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit[] hits = Physics.BoxCastAll(transform.position + new Vector3(0, 0.4f, -3), new Vector3(2f, 1f, 4f), -Vector3.up);
+        foreach (RaycastHit r in hits)
+        {
+            if (r.distance < 1f && !r.collider.gameObject.CompareTag("Player"))
+            {
+                Debug.Log(r.collider.gameObject);
+                rigidbody.AddForce(Vector3.up * 30);
+            }
+        }
+    }
+}
