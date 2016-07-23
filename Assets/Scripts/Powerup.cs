03b6644fc344d153b5eb21fd2cbceb25a660c101
TadCordle <tcordle1@umbc.edu>
2016-07-23 03:26:38 -0400

    Add powerup panel thing that doesn't actually power you up yet

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+    private HingeJoint joint;
+    private float downTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        joint = GetComponent<HingeJoint>();
+        joint.anchor = new Vector3(0f, -0.5f, 0f);
+        joint.axis = transform.right;
+        joint.connectedAnchor = transform.position - Vector3.up;
+        joint.useSpring = false;
+        JointSpring spring = joint.spring;
+        spring.targetPosition = 0;
+        spring.spring = 100;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!joint.useSpring)
+        {
+            downTime -= Time.deltaTime;
+            if (downTime <= 0f)
+            {
+                joint.useSpring = true;
+            }
+        }
+	}
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            downTime = 4f;
+            joint.useSpring = false;
+        }
+    }
+}
