7bbd9c49b0dc573610c85d2406b592c5fedbf06e
TadCordle <tcordle1@umbc.edu>
2016-07-22 23:54:32 -0400

    Work on ship controls

--- Assets/Scripts/ShipControls.cs
+++ Assets/Scripts/ShipControls.cs
@@ -3,25 +3,69 @@ using System.Collections;
 
 public class ShipControls : MonoBehaviour {
 
+    bool fly;
     new Rigidbody rigidbody;
+    float otherY;
 
 	// Use this for initialization
 	void Start ()
     {
+        fly = false;
         rigidbody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        otherY = 0;
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
-        RaycastHit[] hits = Physics.BoxCastAll(transform.position + new Vector3(0, 0.4f, -3), new Vector3(2f, 1f, 4f), -Vector3.up);
-        foreach (RaycastHit r in hits)
+        if (fly)
         {
-            if (r.distance < 1f && !r.collider.gameObject.CompareTag("Player"))
-            {
-                Debug.Log(r.collider.gameObject);
-                rigidbody.AddForce(Vector3.up * 30);
-            }
+            rigidbody.AddForce(-Physics.gravity);
+            rigidbody.AddForce(Vector3.up * 10 * (1 - (transform.position.y - otherY)));
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.angularVelocity = Vector3.zero;
+            if (Vector3.Dot(rigidbody.velocity, -transform.forward) < 30)
+                rigidbody.AddForce(-transform.forward * 40);
+
+        }
+        if (Vector3.Dot(rigidbody.velocity, -transform.forward) > 30)
+            rigidbody.velocity = rigidbody.velocity.normalized * 30;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up * -3);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * 3);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (Vector3.Dot(-rigidbody.velocity, -transform.forward) < 30)
+                rigidbody.AddForce(transform.forward * 40);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            fly = true;
+            otherY = other.transform.position.y;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            fly = false;
+            otherY = other.transform.position.y;
         }
     }
 }
