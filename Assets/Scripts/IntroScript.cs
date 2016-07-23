f9cf6399e78b5b18c07aa274659edd3cddd5ceb2
TadCordle <tcordle1@umbc.edu>
2016-07-23 14:11:19 -0400

    Started intro menu

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class IntroScript : MonoBehaviour {
+
+    public bool playFlag;
+    public AudioSource introIntro;
+    public AudioSource introLooped;
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform.parent.GetComponent<Animator>().Play(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (playFlag)
+        {
+            introIntro.Stop();
+            introLooped.Play();
+            playFlag = false;
+        }
+	}
+
+    public void ClickPlay()
+    {
+        SceneManager.LoadScene("Scenes/Kit/select", LoadSceneMode.Single);
+    }
+}
