29af5d626634845d32b92b83a7d7bc24ed49abab
SATYR\kithe_000 <heckman1@umbc.edu>
2016-07-24 01:54:41 -0400

    The End!

---
+++ Assets/tran.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class tran : MonoBehaviour {
+    public Image[] images;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+    public void showAll(int whoWon)
+    {
+        images[0].color = new Color(1, 1, 1, 1);
+        images[1].color = new Color(1, 1, 1, 1);
+        images[whoWon + 2].color = new Color(1, 1, 1, 1);
+    }
+}
