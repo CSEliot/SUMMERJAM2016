757bda23139c5627ed93c2664c6b9fa2efe087e8
unknown <eliot2@ECS005APC07.ad.umbc.edu>
2016-07-24 02:50:03 -0400

    Commit early and commit often. Working on auto-adjustments for spawning face.

---
+++ Assets/Scripts/GameAdjustments.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameAdjustments : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		adjustRotations ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void adjustRotations(){
+		
+		if (transform.GetChild (0).name.Contains ("Ship")) {
+			Quaternion tempQ = Quaternion.Euler (new Vector3 (0, 180f, 0));
+			transform.localRotation.Set (tempQ.x, tempQ.y, tempQ.z, tempQ.w);
+		}
+		transform.GetChild (0).transform.parent = null;
+	}
+}
