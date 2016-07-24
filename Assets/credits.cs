86abdbbf48835fde302f190cb47b6a554051a6a8
SATYR\kithe_000 <heckman1@umbc.edu>
2016-07-24 05:08:45 -0400

    the end!

---
+++ Assets/credits.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class credits : MonoBehaviour {
+    public Text Text1;
+    private AudioSource sound1;
+    public AudioClip credit;
+
+    // Use this for initialization
+    void Start () {
+        sound1 = GetComponent<AudioSource>();
+        sound1.clip = credit;
+        sound1.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Text1.rectTransform.anchoredPosition += Vector3.up * 10.0F;
+
+    }
+}
