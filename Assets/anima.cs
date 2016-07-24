216f10f0919c2bc4b3f5850385eea425217bc912
SATYR\kithe_000 <heckman1@umbc.edu>
2016-07-24 01:15:48 -0400

    workin' on fanale

---
+++ Assets/anima.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anima : MonoBehaviour {
+    private AudioSource sound1;
+    public AudioClip aniSound;
+    // Use this for initialization
+    void Start () {
+        sound1 = GetComponent<AudioSource>();
+        sound1.clip = aniSound;
+        sound1.Play();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
