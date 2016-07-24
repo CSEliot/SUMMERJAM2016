c49b79499fe0614fe14a3a0135f1cf59127389a3
SATYR\kithe_000 <heckman1@umbc.edu>
2016-07-24 14:32:24 -0400

    stage select

--- Assets/stageselect.cs
+++ Assets/stageselect.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class stageselect : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+    private AudioSource sound1;
+    private int timeLeft = 50;
+    private bool gonow = false;
+    public AudioClip selectSound;
+    public Image[] Images;
+    // Use this for initialization
+    void Start () {
+        Debug.Log("faaab");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.anyKeyDown)
+        {
+            sound1 = GetComponent<AudioSource>();
+            sound1.clip = selectSound;
+            sound1.Play();
+            for (int n = 0; n < 3; n += 1)
+            {
+                Images[n].color = new Color(1, 1, 1, 1);
+                gonow = true;
+                Debug.Log("sofarsogood!");
+            }
+        }
+            if (gonow == true) {
+                Debug.Log("wtf");
+                timeLeft -= 1;
+                if (timeLeft < 0)
+                {
+                    Debug.Log("work");
+                }
+            }
+    }
 }
