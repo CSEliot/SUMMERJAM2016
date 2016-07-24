791af54a5812a942e16de21796d3ae535ab3f24a
TadCordle <tcordle1@umbc.edu>
2016-07-24 12:26:44 -0400

    90's

---
+++ Assets/UIShenanigans.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIShenanigans : MonoBehaviour {
+
+    public Text[] allText;
+    public Font[] allFonts;
+    public Color[] allColors;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.Range(0, 2) == 1) return;
+
+        foreach (Text t in allText)
+        {
+            t.font = allFonts[Random.Range(0, allFonts.Length)];
+            t.color = allColors[Random.Range(0, allColors.Length)];
+            t.fontStyle = (Random.Range(0, 3) == 0 ? FontStyle.Bold : (Random.Range(0, 3) == 0 ? FontStyle.Italic : FontStyle.BoldAndItalic));
+        }
+	}
+}
